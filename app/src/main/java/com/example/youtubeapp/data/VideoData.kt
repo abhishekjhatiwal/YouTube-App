f65590749c22078b4735ec35d776ff33a8fb718c
Abhishekverma2006 <j472812@gmail.com>
2025-10-30 21:46:17 +0530

    30-10-2025 21.46

--- app/src/main/java/com/example/youtubeapp/data/VideoData.kt
+++ app/src/main/java/com/example/youtubeapp/data/VideoData.kt
@@ -1,7 +1,19 @@
 package com.example.youtubeapp.data
 
+import android.net.Uri
 data class VideoData(
     val videoUrl : String,
     val videoName : String,
     val storagePath : String
 )
+
+sealed class Screen(val route: String) {
+    object Home : Screen("home")
+    object VideoPlayer : Screen("video_player/{videoUrl}/{videoName}/{storagePath}") {
+        fun createRoute(videoUrl: String, videoName: String, storagePath: String): String {
+            val encodedUrl = Uri.encode(videoUrl)
+            val encodedPath = Uri.encode(storagePath)
+            return "video_player/$encodedUrl/$videoName/$encodedPath"
+        }
+    }
+}
--- app/src/main/java/com/example/youtubeapp/screen/VideoItem.kt
+++ app/src/main/java/com/example/youtubeapp/screen/VideoItem.kt
@@ -3,6 +3,7 @@ package com.example.youtubeapp.screen
 import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
